14537fb4406ae42cb5f59509a23fa44883aaf496
Roman Joeres <Roman.Joeres@helmholtz-hzi.de>
2022-09-18 11:52:46 +0200

    data folder of CIN

---
+++ src/models/cin/data/datasets/__init__.py
@@ -0,0 +1,13 @@
+from src.models.cin.data.datasets.dataset import ComplexDataset, InMemoryComplexDataset
+from src.models.cin.data.datasets.sr import SRDataset, load_sr_graph_dataset
+from src.models.cin.data.datasets.cluster import ClusterDataset
+from src.models.cin.data.datasets.tu import TUDataset, load_tu_graph_dataset
+from src.models.cin.data.datasets.flow import FlowDataset
+from src.models.cin.data.datasets.ocean import OceanDataset
+from src.models.cin.data.datasets.zinc import ZincDataset, load_zinc_graph_dataset
+from src.models.cin.data.datasets.dummy import DummyDataset, DummyMolecularDataset
+from src.models.cin.data.datasets.csl import CSLDataset
+from src.models.cin.data.datasets.ogb import OGBDataset, load_ogb_graph_dataset
+from src.models.cin.data.datasets.ringtransfer import RingTransferDataset, load_ring_transfer_dataset
+from src.models.cin.data.datasets.ringlookup import RingLookupDataset, load_ring_lookup_dataset
+
